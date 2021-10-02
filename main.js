75455f5d4d36fd627d78c26ccd0d47cadbf72705
iamigrek <gerevyanko.igorek@gmail.com>
2021-10-03 01:21:31 +0300

    Доделал функционал

--- main.js
+++ main.js
@@ -1,127 +1,71 @@
-// const button = document.querySelectorAll('[data-main-btn]');
-// const buttonClass = document.querySelectorAll('.game__btn');
-// const status = document.querySelector('.status');
+const gameBtn = document.querySelectorAll('[data-pos]');
+const gameBtnClear = document.querySelector('.game__clear');
+const gameBtnY = document.querySelectorAll('[data-y]');
+const gameBtnX = document.querySelectorAll('[data-x]');
+const gameBtnPos = document.querySelectorAll('[data-pos]');
+let i = 0;
 
-// let i = 0;
-
-// let def = [];
-// let win = [1, 1, 1, 0, 0, 0, 0, 0, 0];
-
-// button.forEach(function (item) {
-//   item.addEventListener('click', function (e) {
-//     item.textContent = `${i % 2 == 0 ? 'O' : 'X'}`;
-//     item.value = `${i % 2 == 0 ? 2 : 1}`;
-
-//     def[this.dataset.mainBtn] = `${i % 2 == 0 ? 1 : 0}`;
-//     i++;
-//     console.log(def);
-//     if (def == win) {
-//       alert('WIN!');
-//     }
-//   });
-// });
-
-// const button = document.querySelectorAll('[data-main-btn]');
-// const buttonClass = document.querySelectorAll('.game__btn');
-// const status = document.querySelector('.status');
-
-// let i = 0;
-
-// let def = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-
-// var winIndex = [
-//   [1, 1, 1],
-//   [4, 5, 6],
-//   [7, 8, 9],
-//   [1, 4, 7],
-//   [2, 5, 8],
-//   [3, 6, 9],
-//   [1, 5, 9],
-//   [3, 5, 7],
-// ];
-
-// button.forEach(function (item) {
-//   item.addEventListener('click', function (e) {
-//     item.textContent = `${i % 2 == 0 ? 'O' : 'X'}`;
-//     item.value = `${i % 2 == 0 ? 2 : 1}`;
-
-//     def[this.dataset.mainBtn] = i % 2 == 0 ? 1 : 0;
-//     i++;
-//     //
-//     for (var k in winIndex) {
-//       var win = true;
-//       for (var j in winIndex[k]) {
-//         var id = winIndex[k][j];
-//         var ind = def.indexOf(id);
-
-//         if (ind == -1) {
-//           win = false;
-//         }
-//       }
-
-//       if (win) console.log('win');
-//     }
-//   });
-// });
-
-// ========================
-
-const button = document.querySelectorAll('[data-y]');
-const buttonClass = document.querySelectorAll('.game__btn');
-const status = document.querySelector('.status');
-
-let i = 1;
-
-let defx = [
+const gamerX = [
   [0, 0, 0],
   [0, 0, 0],
   [0, 0, 0],
 ];
 
-let defo = [
+const gamerO = [
   [0, 0, 0],
   [0, 0, 0],
   [0, 0, 0],
 ];
 
-var winIndex = [
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 9],
-  // [1, 4, 7],
-  // [2, 5, 8],
-  // [3, 6, 9],
-  // [1, 5, 9],
-  // [3, 5, 7],
-];
-
-button.forEach(function (item) {
-  item.addEventListener('click', function (e) {
-    item.textContent = `${i % 2 == 0 ? 'O' : 'X'}`;
-    item.value = `${i % 2 == 0 ? 2 : 1}`;
+gameBtn.forEach(element => {
+  element.addEventListener('click', function () {
+    i % 2 == 0 ? (element.textContent = 'X') : (element.textContent = 'O');
 
     i % 2 == 0
-      ? (defx[this.dataset.y][this.dataset.x] = parseInt(this.dataset.pos))
-      : (defo[this.dataset.y][this.dataset.x] = parseInt(this.dataset.pos));
-
-    console.log(defx);
-    console.log(defo);
-    for (let k = 0; k < 3; k++) {
-      for (let l = 0; l < 3; l++) {
-        console.log('===/===');
-        console.log(defx[k]);
-
-        console.log(winIndex[l]);
-        console.log('===/===');
-        if (defx[k].join() == winIndex[l].join()) {
-          //для def нужно использовать двухмерный массив
-          alert('O WIN!!');
-        }
-        if (defo[k].join() == winIndex[l].join()) {
-          alert('X WIN!!');
-        }
-      }
-    }
+      ? (gamerX[element.dataset.y][element.dataset.x] = parseInt(
+          element.dataset.pos
+        ))
+      : (gamerO[element.dataset.y][element.dataset.x] = parseInt(
+          element.dataset.pos
+        ));
     i++;
+
+    if (
+      gamerX[0][0] * gamerX[0][1] * gamerX[0][2] == 6 ||
+      gamerX[1][0] * gamerX[1][1] * gamerX[1][2] == 120 ||
+      gamerX[2][0] * gamerX[2][1] * gamerX[2][2] == 504 ||
+      gamerX[0][0] * gamerX[1][0] * gamerX[2][0] == 28 ||
+      gamerX[0][1] * gamerX[1][1] * gamerX[2][1] == 80 ||
+      gamerX[0][2] * gamerX[1][2] * gamerX[2][2] == 162 ||
+      gamerX[0][0] * gamerX[1][1] * gamerX[2][2] == 45 ||
+      gamerX[0][2] * gamerX[1][1] * gamerX[2][0] == 105
+    ) {
+      alert('WIN X');
+      clear();
+    } else if (
+      gamerO[0][0] * gamerO[0][1] * gamerO[0][2] == 6 ||
+      gamerO[1][0] * gamerO[1][1] * gamerO[1][2] == 120 ||
+      gamerO[2][0] * gamerO[2][1] * gamerO[2][2] == 504 ||
+      gamerO[0][0] * gamerO[1][0] * gamerO[2][0] == 28 ||
+      gamerO[0][1] * gamerO[1][1] * gamerO[2][1] == 80 ||
+      gamerO[0][2] * gamerO[1][2] * gamerO[2][2] == 162 ||
+      gamerO[0][0] * gamerO[1][1] * gamerO[2][2] == 45 ||
+      gamerO[0][2] * gamerO[1][1] * gamerO[2][0] == 105
+    ) {
+      alert('WIN O');
+      clear();
+    }
   });
 });
+
+gameBtnClear.addEventListener('click', clear);
+
+function clear() {
+  for (let f = 0; f < 3; f++) {
+    gamerO[f] = [0, 0, 0];
+    gamerX[f] = [0, 0, 0];
+    gameBtn.forEach(item => {
+      item.textContent = '';
+    });
+  }
+}
